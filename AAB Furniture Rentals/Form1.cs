cbb151a690080d739c48330299ddd10b1ba7c159
akernan2 <akernan2@CSVLAB-VM11>
2022-03-07 17:48:51 -0500

    Add project files.

---
+++ AAB Furniture Rentals/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAB_Furniture_Rentals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
