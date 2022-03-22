5d7ff50ddc63e733535fc00d135e337d2c9263c0
Blair Pattison <blair.pattis@gmail.com>
2022-03-22 16:55:49 -0700

    Created main dashboard for Admin, added a TabForm to it and a Logout label and a Name label.

---
+++ AAB Furniture Rentals/View/AdminMainDashboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAB_Furniture_Rentals.View
+{
+    public partial class AdminMainDashboard : Form
+    {
+        public AdminMainDashboard()
+        {
+            InitializeComponent();
+        }
+
+  
+    }
+}
