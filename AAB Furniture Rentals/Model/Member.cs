9fc1b5e9744dc684e2e3bcfa5310f3f94d29cde5
Blair Pattison <blair.pattis@gmail.com>
2022-03-23 11:39:38 -0700

    Created Rental, Member, and Furniture classes with getters and setters for their respective data.

---
+++ AAB Furniture Rentals/Model/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAB_Furniture_Rentals.Model
+{
+    class Member
+    {
+        int memberID { get; set; }
+        int firstName { get; set; }
+        int lastName { get; set; }
+        char gender { get; set; }
+        string dateOfBirth { get; set; }
+        string address { get; set; }
+        string phoneNumber { get; set; }
+
+    }
+}
