d2e03981df89e09c74b0bac1343a33ea8b7ad12b
akernan2 <akernan2@CSVLAB-VM11>
2022-03-26 01:28:10 -0400

    Saving Progress. Database VM seems to be working now, and is faster.

--- AAB Furniture Rentals/DAL/EmployeesDAL.cs
+++ AAB Furniture Rentals/DAL/EmployeesDAL.cs
@@ -29,36 +29,49 @@ namespace AAB_Furniture_Rentals.DAL
                 {
                     using (SqlDataReader reader = selectCommand.ExecuteReader())
                     {
-                        int name = reader.GetOrdinal("Name");
-                        int EmployeeID = reader.GetOrdinal("EmployeeID");
-                        int address = reader.GetOrdinal("Address");
-                        int city = reader.GetOrdinal("City");
-                        int state = reader.GetOrdinal("State");
-                        int zip = reader.GetOrdinal("ZipCode");
-                        int phone = reader.GetOrdinal("Phone");
-                        int email = reader.GetOrdinal("Email");
+                        int employeeID = reader.GetOrdinal("employeeID");
+                        int fname = reader.GetOrdinal("fname");
+                        int lname = reader.GetOrdinal("lname");
+                        int sex = reader.GetOrdinal("sex");
+                        int dob = reader.GetOrdinal("dob");
+                        int address = reader.GetOrdinal("address");
+                        int city = reader.GetOrdinal("city");
+                        int state = reader.GetOrdinal("state");
+                        int zip = reader.GetOrdinal("zip");
+                        int phone = reader.GetOrdinal("phone");
+                        int active = reader.GetOrdinal("active");
+                        int login_data_username = reader.GetOrdinal("login_data_username");
 
                         while (reader.Read())
                         {
 
-                            string _name = reader.GetString(name);
-                            int _EmployeeID = reader.GetInt32(EmployeeID);
+                            string _employeeID = reader.GetString(employeeID);
+                            string _fname = reader.GetString(fname);
+                            string _lname = reader.GetString(lname);
+                            string _sex = reader.GetString(sex);
+                            DateTime _dob = reader.GetDateTime(dob);
                             string _address = reader.GetString(address);
                             string _city = reader.GetString(city);
                             string _state = reader.GetString(state);
                             string _zip = reader.GetString(zip);
                             string _phone = reader.GetString(phone);
-                            string _email = reader.GetString(email);
+                            bool _active = reader.GetBoolean(active);
+                            string _login_data_username = reader.GetString(login_data_username);
+
 
                             Employee Employee = new Employee(
-                                name: _name,
-                                EmployeeID: _EmployeeID,
-                                address: _address,
-                                city: _city,
-                                state: _state,
-                                zip: _zip,
-                                phone: _phone,
-                                email: _email
+                                employeeID: _employeeID.Trim(),
+                                fname: _fname.Trim(),
+                                lname: _lname.Trim(),
+                                sex: _sex.Trim(),
+                                dob: _dob,
+                                address: _address.Trim(),
+                                city: _city.Trim(),
+                                state: _state.Trim(),
+                                zip: _zip.Trim(),
+                                phone: _phone.Trim(),
+                                active: _active,
+                                login_data_username: _login_data_username.Trim()
                                 );
 
                             EmployeeList.Add(Employee);
--- AAB Furniture Rentals/Model/Employee.cs
+++ AAB Furniture Rentals/Model/Employee.cs
@@ -8,8 +8,33 @@ namespace AAB_Furniture_Rentals.Model
 {
     class Employee
     {
+        public string employeeID { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string sex { get; set; }
+        public DateTime dob { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phone { get; set; }
+        public bool active { get; set; }
+        public string login_data_username { get; set; }
 
-        public Employee()
+        public Employee(
+            string employeeID,
+            string fname,
+            string lname,
+            string sex,
+            DateTime dob,
+            string address,
+            string city,
+            string state,
+            string zip,
+            string phone,
+            bool active,
+            string login_data_username
+            )
         {
         }
     }
