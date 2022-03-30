66ad908111e7ddb07e89129e75366655c89cc3af
akernan2 <akernan2@CSDBLAB-VM16>
2022-03-30 16:44:56 -0400

    Created Controller, Model, And DAL for the furniture table

---
+++ AAB Furniture Rentals/DAL/FurnitureDAL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AAB_Furniture_Rentals.Model;
+
+namespace AAB_Furniture_Rentals.DAL
+{
+    public class FurnitureDAL
+    {
+
+        /// <summary>
+        /// Gets all Furnitures.
+        /// </summary>
+        /// <returns>A list of all Furnitures </returns>
+        public List<Furniture> GetAllFurniture()
+        {
+
+            List<Furniture> FurnitureList = new List<Furniture>();
+
+            string selectStatement = "SELECT * FROM furniture";
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        var furnitureID = reader.GetOrdinal("furnitureID");
+                        var style_type = reader.GetOrdinal("style_type");
+                        var category_type = reader.GetOrdinal("category_type");
+                        var description = reader.GetOrdinal("description");
+                        var daily_rental_rate = reader.GetOrdinal("daily_rental_rate");
+                        var quantity = reader.GetOrdinal("quantity");
+                        var fine_rate = reader.GetOrdinal("fine_rate");
+
+                        while (reader.Read())
+                        {
+
+                            int _furnitureID = reader.GetInt32(furnitureID);
+                            string _style_type = reader.GetString(style_type);
+                            string _category_type = reader.GetString(category_type);
+                            string _description = reader.GetString(description);
+                            double _daily_rental_rate = reader.GetDouble(daily_rental_rate);
+                            int _quantity = reader.GetInt32(quantity);
+                            double _fine_rate = reader.GetDouble(fine_rate);
+                          
+
+                            Furniture Furniture = new Furniture(
+                                furnitureID: _furnitureID,
+                                style: _style_type.Trim(),
+                                category: _category_type.Trim(),
+                                description: _description.Trim(),
+                                dailyRentalRate: _daily_rental_rate,
+                                quantityOnHand: _quantity,
+                                fineRate: _fine_rate
+                               
+                                );
+
+                            FurnitureList.Add(Furniture);
+                        }
+                    }
+                }
+            }
+
+            return FurnitureList;
+        }
+
+
+    }
+}
---
+++ AAB Furniture Rentals/Model/Furniture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAB_Furniture_Rentals.Model
+{
+    public class Furniture
+    {
+        public int FurnitureID { get; set; }
+        public string Style { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double DailyRentalRate { get; set; }
+        public int QuantityOnHand { get; set; }
+        public double FineRate { get; set; }
+
+
+        public Furniture(
+            int furnitureID,
+            string style,
+            string category,
+            string description,
+            double dailyRentalRate,
+            int quantityOnHand,
+            double fineRate
+           
+            )
+        {
+            this.FurnitureID = furnitureID;
+            this.Style = style;
+            this.Category = category;
+            this.Description = description;
+            this.DailyRentalRate = dailyRentalRate;
+            this.QuantityOnHand = quantityOnHand;
+            this.FineRate = fineRate;
+
+
+
+
+
+        }
+    }
+}
