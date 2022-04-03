500c0160273174e85f166aca928734b331441299
Ashley Nicholson <an00047@CSDBLAB-VM10>
2022-04-03 18:18:46 -0400

    Initialize GUI layout

---
+++ AAB Furniture Rentals/View/UserControls/EmployeeRentalsUserControl.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace AAB_Furniture_Rentals.UserControls
+{
+    partial class EmployeeRentalsUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerTransactionDataGridView = new System.Windows.Forms.DataGridView();
+            this.searchComboBox = new System.Windows.Forms.ComboBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.customerTransactionDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerTransactionDataGridView
+            // 
+            this.customerTransactionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customerTransactionDataGridView.Location = new System.Drawing.Point(77, 76);
+            this.customerTransactionDataGridView.Name = "customerTransactionDataGridView";
+            this.customerTransactionDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.customerTransactionDataGridView.TabIndex = 0;
+            // 
+            // searchComboBox
+            // 
+            this.searchComboBox.FormattingEnabled = true;
+            this.searchComboBox.Location = new System.Drawing.Point(171, 38);
+            this.searchComboBox.Name = "searchComboBox";
+            this.searchComboBox.Size = new System.Drawing.Size(80, 21);
+            this.searchComboBox.TabIndex = 1;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(65, 38);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(257, 38);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // EmployeeRentalsUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchComboBox);
+            this.Controls.Add(this.customerTransactionDataGridView);
+            this.Name = "EmployeeRentalsUserControl";
+            this.Size = new System.Drawing.Size(400, 250);
+            ((System.ComponentModel.ISupportInitialize)(this.customerTransactionDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView customerTransactionDataGridView;
+        private System.Windows.Forms.ComboBox searchComboBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
