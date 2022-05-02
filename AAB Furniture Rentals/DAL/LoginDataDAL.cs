08064632d7e57e5f80b27e403ba71e9358e6c34c
Ashley Nicholson <an00047@CSDBLAB-VM10>
2022-05-02 15:40:28 -0400

    Create quuery for updating and inserting

--- AAB Furniture Rentals/DAL/LoginDataDAL.cs
+++ AAB Furniture Rentals/DAL/LoginDataDAL.cs
@@ -8,13 +8,57 @@ namespace AAB_Furniture_Rentals.DAL
     /// </summary>
     class LoginDataDAL
     {
-        public void UpdateUsernameAndPassword(string newUsername, string newPassword, int employeeID)
+        public void UpdateUsernameAndPassword(string newUsername, string newPassword, string oldUsername)
         {
-            string query = "UPDATE employee SET " +
+            if (newPassword != "")
+            {
+                string query = "UPDATE login_data SET " +
+
+                    "username = @username, " +
+                    "password = @password, " +
+                    "WHERE username = @oldUsername ";
+
+                using (SqlConnection connection = RentMeDBConnection.GetConnection())
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+
+                    {
+                        command.Parameters.AddWithValue("@oldUsername", oldUsername);
+                        command.Parameters.AddWithValue("@username", newUsername);
+                        command.Parameters.AddWithValue("@password", newPassword);
+                        command.ExecuteScalar();
+
+                    }
+                }
+            } else
+            {
+                string queryUsername = "UPDATE login_data SET " +
+
+                    "username = @username, " +
+                    "WHERE username = @oldUsername ";
+
+                using (SqlConnection connection = RentMeDBConnection.GetConnection())
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(queryUsername, connection))
+
+                    {
+                        command.Parameters.AddWithValue("@oldUsername", oldUsername);
+                        command.Parameters.AddWithValue("@username", newUsername);
+                        command.ExecuteScalar();
+
+                    }
+                }
+            }
+        }
+
+        public void InsertNewLoginData(string username, string password)
+        {
+            string query = "INSERT INTO login_data " +
 
                 "username = @username, " +
-                "password = @password, " +
-                "WHERE employeeID=@EMPLOYEE_ID ";
+                "password = @password, ";
 
             using (SqlConnection connection = RentMeDBConnection.GetConnection())
             {
@@ -22,9 +66,8 @@ namespace AAB_Furniture_Rentals.DAL
                 using (SqlCommand command = new SqlCommand(query, connection))
 
                 {
-                    command.Parameters.AddWithValue("@EMPLOYEE_ID", employeeID);
-                    command.Parameters.AddWithValue("@username", newUsername);
-                    command.Parameters.AddWithValue("@password", newPassword);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
                     command.ExecuteScalar();
 
                 }
